2bf8aed1fe5288a9e65c0ff21f3940ea3d8035c1
Rahmaruuxi <rahmaruun442@gmail.com>
2025-07-04 10:40:45 +0300

    Initial commit

---
+++ src/pages/dashboard/projects.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Projects = () => {
+  return <div>projectP</div>;
+};
+
+export default Projects;
---
+++ src/pages/dashboard/tasks.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tasks = () => {
+  return <div>T</div>;
+};
+
+export default Tasks;
